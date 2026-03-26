10e57bebea3aeebc7bc008d29ac567877a9b2a3a
Andres Moreno <andresmoreno1991609@gmail.com>
2026-03-26 15:52:31 -0500

    first commit

---
+++ src/test/java/com/saferoute/SaferouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saferoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaferouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
