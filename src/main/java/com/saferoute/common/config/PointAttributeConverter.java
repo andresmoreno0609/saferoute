998c923529f7756ef30c3318cc4f414228ae678e
Andres Moreno <andresmoreno1991609@gmail.com>
2026-05-02 13:41:59 -0500

    fix: correct PointAttributeConverter with WKTReader

--- src/main/java/com/saferoute/common/config/PointAttributeConverter.java
+++ src/main/java/com/saferoute/common/config/PointAttributeConverter.java
@@ -3,6 +3,7 @@ package com.saferoute.common.config;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.io.WKTReader;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
@@ -13,6 +14,7 @@ import jakarta.persistence.Converter;
 public class PointAttributeConverter implements AttributeConverter<Point, String> {
 
     private static final GeometryFactory GF = new GeometryFactory();
+    private static final WKTReader READER = new WKTReader(GF);
 
     @Override
     public String convertToDatabaseColumn(Point point) {
@@ -28,10 +30,8 @@ public class PointAttributeConverter implements AttributeConverter<Point, String
             return null;
         }
         try {
-            return GF.read(wkt).getFactory().createPoint(new Coordinate(
-                    ((Point) GF.read(wkt)).getX(),
-                    ((Point) GF.read(wkt)).getY()
-            ));
+            Point parsed = (Point) READER.read(wkt);
+            return GF.createPoint(new Coordinate(parsed.getX(), parsed.getY()));
         } catch (Exception e) {
             return null;
         }
